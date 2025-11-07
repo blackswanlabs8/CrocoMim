fd1672c6011daa5eaccd2837cc79c040fd2f427a
blackswanlabs8 <povi@yandex.ru>
2025-11-07 17:14:07 +0300

    Update team and help buttons

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // тест
-const CACHE_NAME = 'crocomim-static-v3';
-const DICTS_CACHE = 'crocomim-dicts-runtime';
+const CACHE_NAME = 'crocomim-static-v5';
+const DICTS_CACHE = 'crocomim-dicts-runtime-v2';
 const ASSETS = [
   './',
   './index.html',
